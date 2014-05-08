1b6e32f70a969f4561406f1bc4bcf54e0dc2c661
Jacob Jensen <jjonj@hotmail.com>
2014-05-08 13:34:34 +0200

    Initial commit.

---
+++ qkReader/qkReader/MainView.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace qkReader
+{
+    partial class MainView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sourceTextField = new System.Windows.Forms.RichTextBox();
+            this.wordField = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sourceTextField
+            // 
+            this.sourceTextField.Location = new System.Drawing.Point(237, 304);
+            this.sourceTextField.Name = "sourceTextField";
+            this.sourceTextField.Size = new System.Drawing.Size(396, 213);
+            this.sourceTextField.TabIndex = 0;
+            this.sourceTextField.Text = "";
+            // 
+            // wordField
+            // 
+            this.wordField.AutoSize = true;
+            this.wordField.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wordField.Location = new System.Drawing.Point(381, 104);
+            this.wordField.Name = "wordField";
+            this.wordField.Size = new System.Drawing.Size(88, 39);
+            this.wordField.TabIndex = 1;
+            this.wordField.Text = "word";
+            // 
+            // MainView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OldLace;
+            this.ClientSize = new System.Drawing.Size(888, 529);
+            this.Controls.Add(this.wordField);
+            this.Controls.Add(this.sourceTextField);
+            this.Name = "MainView";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox sourceTextField;
+        private System.Windows.Forms.Label wordField;
+    }
+}
+
---
+++ qkReader/qkReader/Reader.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace qkReader
+{
+    /// <summary>
+    ///  This class is responsible for controlling active text and forwarding the displaying of it.
+    /// </summary>
+    class Reader
+    {
+        #region constants
+
+        /// <summary>
+        ///  The duration for which the first displayed word will stay before starting.
+        /// </summary>
+        private static double START_WORD_INTERVAL = 500;
+
+        /// <summary>
+        ///  The multiple of maxWPM that is added to the currentWPM until it reaches top speed.
+        ///  Note that the speedup will be exponential.
+        /// </summary>
+        private static double WPM_ACCELERATION_FACTOR = 0.1;
+
+        #endregion
+
+
+        #region MemberFunctions
+
+        public Reader(TextSource textSource, TextRenderer textRenderer, double maxWPM)
+        {
+            this.maxWPM = maxWPM;
+            this.textRenderer = textRenderer;
+            this.textSource = textSource;
+
+            this.wordTimer = new Timer(START_WORD_INTERVAL);
+            wordTimer.Elapsed += new ElapsedEventHandler(wordTimerHandler);
+            
+        }
+
+        public void start(bool accelerate)
+        {
+            if (accelerate)
+            {
+                currentWPM = 60000 / START_WORD_INTERVAL; // 60 seconds divided by the first word duration
+            }
+            else
+            {
+                currentWPM = maxWPM;
+            }
+
+            wordTimer.Start();
+        }
+
+        /// <summary>
+        ///  Pauses the reader at the current word. The reader can be resumed with call to start.
+        /// </summary>
+        public void pause()
+        {
+            wordTimer.Stop();
+        }
+
+        /// <summary>
+        ///  Completely stops the reader. The displayed text will be set to default and 
+        ///  subsequent start calls will start the reader from the beginning.
+        /// </summary>
+        public void stop()
+        {
+            wordTimer.Stop();
+        }
+
+        /// <summary>
+        ///  Returns the current WPM of the reader.
+        /// </summary>
+        public double getCurrentWPM()
+        {
+            return currentWPM;
+        }
+
+        /// <summary>
+        ///  Eventhandler for the wordTimer. This is where the actual text logic happens.
+        /// </summary>
+        public void wordTimerHandler(object source, ElapsedEventArgs e)
+        {
+            if (currentWPM < maxWPM) 
+            {
+                wordTimer.Interval += maxWPM * WPM_ACCELERATION_FACTOR; 
+            }
+            textRenderer.displayText( textSource.getNextWord() );
+        }
+
+        #endregion
+
+
+
+        #region MemberVariables
+
+        /// <summary>
+        ///  The top speed in Words Per Minute that this reader will run at
+        /// </summary>
+        private double maxWPM;
+
+        /// <summary>
+        ///  The current WPM speed
+        /// </summary>
+        private double currentWPM;
+
+        /// <summary>
+        ///  The source of the text being read
+        /// </summary>
+        private TextSource textSource;
+
+        /// <summary>
+        ///  Object responsible for accepting text-rendering operations chosen by this reader
+        /// </summary>
+        private TextRenderer textRenderer;
+
+        /// <summary>
+        ///  Timer used for switching between words.
+        /// </summary>
+        private Timer wordTimer;
+
+        #endregion
+
+    }
+}
